56a5c3005ceb1289898b656121b66b2425420da6
ChetouiArij11 <arijchtoui1@gmail.com>
2022-11-03 22:25:28 +0100

    version 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EleveComponent } from './eleve/eleve.component';
+import { ProfComponent } from './prof/prof.component';
+import { AdministrationComponent } from './administration/administration.component';
+import { HeaderComponent } from './header/header.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EleveComponent,
+    ProfComponent,
+    AdministrationComponent,
+    HeaderComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
